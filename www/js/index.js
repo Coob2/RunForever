bced0b7aea8ec1ed61dfd540218872ad88071dd7
Coob2 <32450886+Coob2@users.noreply.github.com>
2018-05-10 15:25:03 +0100

    Edited class of each page
    Changed class to "scroller"

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,4 @@
-$(document).on("pagecreate","#pageone",function(){
+$(document).on("pagecreate","#home",function(){
   $('#submitButton').on("click", function(){
     submitText();
   });
@@ -7,8 +7,11 @@ $(document).on("pagecreate","#pageone",function(){
 function submitText() {
 	var text = $('#textinput').val();
 	alert(text);
+  storeValue("name",$('#textinput').val());
 }
 
 function storeValue(key, value) {
-
+  var storage = window.localStorage;
+  var value = storage.getItem(key); // Pass a key name to get its value.
+  storage.setItem(key, value) // Pass a key name and its value to add or update that key.
 }
