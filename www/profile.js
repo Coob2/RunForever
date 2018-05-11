f14821948eaa4cf45716cce316ff8f5edfb37e34
Coob2 <32450886+Coob2@users.noreply.github.com>
2018-05-11 11:07:00 +0100

    completed Profile
    Profile is completed

---
+++ www/profile.js
@@ -0,0 +1,29 @@
+$( document ).ready(function loadData() {
+
+});
+
+function loadData()
+{
+var name = localStorage.getItem("name");
+var age = localStorage.getItem("age");
+var gender = localStorage.getItem("gender");
+
+$('#name').val(name);
+$('#age').val(age);
+$('#gender').val(gender);
+};
+
+function saveData()
+{
+  localStorage.removeItem("name");
+  localStorage.removeItem("age");
+  localStorage.removeItem("gender");
+
+var name = $('#name').val(name);
+var age = $('#age').val(age);
+var gender = $('#gender').val(gender);
+
+  localStorage.setItem('name', name);
+  localStorage.setItem('age', age);
+  localStorage.setItem('gender', gender);
+}
---
+++ www/run.js
@@ -0,0 +1,61 @@
+var watchID;
+
+var locationOptions = {
+	maximumAge: 10000,
+	timeout: 6000,
+	enableHighAccuracy: true
+};
+
+//when the jQuery Mobile page is initialised
+$(document).on('pageinit', initMap() {
+
+	//set up listener for button click
+	$(document).on('click', getPosition);
+
+	//change time box to show message
+	$('#time').val("Press the button to get location data");
+});
+
+$(document).onPause() {
+watchID = navigator.geolocation.watchPosition(successPosition, failPosition, locationOptions);
+}
+
+//get the current possition when location changes
+function getPosition() {
+	watchID = navigator.geolocation.watchPosition(successPosition, failPosition, locationOptions);
+}
+
+var map;
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: -34.397, lng: 150.644},
+		zoom: 8
+	});
+}
+
+function savePosition(){
+
+}
+
+//set location options
+var locationOptions = {
+maximumAge: 3000,
+timeout: 5000,
+enableHighAccuracy: true};
+
+//called when the position is successfully determined
+function successPosition(position) {
+
+	var time = position.timestamp;
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+	var accuracy = position.coords.accuracy;
+
+
+
+}
+
+//called if the position is not obtained correctly
+function failPosition(error) {
+
+}
